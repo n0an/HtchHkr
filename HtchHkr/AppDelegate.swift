18796eedbbdd93a0285ee0f1becb87b33ea83a99
n0an <nantog@gmail.com>
2017-09-08 21:29:27 +0300

    LeftSidePanelVC outlets connected to firebase data

--- HtchHkr/AppDelegate.swift
+++ HtchHkr/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     fileprivate var containerVC: ContainerVC!
     
+    var menuContainerVC: ContainerVC {
+        return containerVC
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         containerVC = ContainerVC()
@@ -50,6 +54,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    class func getAppDelegate() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
 
 }
 
